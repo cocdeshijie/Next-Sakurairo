7fbd3e16950fedec78ce9cdbd62b13a6812a5e78
cocdeshijie <me@qwq.xyz>
2024-05-02 02:08:38 -0400

    get posts api route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,39 @@
+import { posts } from "#site/content";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+
+    // Get the page number (default to 0)
+    const pageQuery = parseInt(searchParams.get("page") || "0", 10);
+
+    // Get the number of posts per page (default to 10)
+    const postsPerPage = parseInt(searchParams.get("postsPerPage") || "10", 2);
+
+    // Calculate start and end indices for pagination
+    const startIndex = pageQuery * postsPerPage;
+    const endIndex = startIndex + postsPerPage;
+
+    // Get the posts for the current page and map them to include only necessary fields
+    const paginatedPosts = posts.slice(startIndex, endIndex).map(post => ({
+        title: post.title,
+        slug: post.slug,
+        date: post.date,
+        cover: post.cover,
+        tags: post.tags,
+        metadata: post.metadata,
+        excerpt: post.excerpt,
+        permalink: post.permalink,
+        edited: post.edited,
+    }));
+
+    // Determine if this is the last page
+    const lastPage = endIndex >= posts.length;
+
+    const data = {
+        "posts": paginatedPosts,
+        "last_page": lastPage
+    };
+
+    return Response.json(data);
+}
