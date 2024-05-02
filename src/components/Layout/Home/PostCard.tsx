274d3b089fa33d63c8f0588f4799933a7f0172b2
cocdeshijie <me@qwq.xyz>
2024-05-02 02:08:22 -0400

    changed link to div since it mess up styling

--- src/components/Layout/Home/PostCard.tsx
+++ src/components/Layout/Home/PostCard.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import {atom, useAtom} from "jotai/index";
-import {cn} from "@/utils/cn";
-import Link from "next/link";
+import { cn } from "@/utils/cn";
+import { useState } from "react";
 
 interface Post {
     title: string;
@@ -18,27 +17,28 @@ interface PostCardProps {
     index: number;
 }
 
-const cardHoverAtom = atom(false);
-
-// TODO: alternate left and right based on even and odd index
 const PostCard = ({ post, index }: PostCardProps) => {
-    const [cardHover, setCardHover] = useAtom(cardHoverAtom);
+    const [isHovered, setIsHovered] = useState(false);
 
     const handleMouseEnter = () => {
-        setCardHover(true);
+        setIsHovered(true);
     };
 
     const handleMouseLeave = () => {
-        setCardHover(false);
+        setIsHovered(false);
+    };
+
+    const handleClick = () => {
+        window.location.href = post.permalink;
     };
 
     return (
-        <Link
-            href={post.permalink}
+        <div
             key={index}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            className={"relative md:h-36 rounded-lg shadow-lg overflow-hidden md:flex"}
+            onClick={handleClick}
+            className={"relative md:h-36 rounded-lg shadow-lg overflow-hidden md:flex cursor-pointer"}
         >
             <div className={"absolute inset-0 z-0 overflow-hidden"}>
                 <Image
@@ -51,14 +51,14 @@ const PostCard = ({ post, index }: PostCardProps) => {
                 />
             </div>
             <div className={"relative z-10 md:w-2/3"}>
-                <div className={"p-6 h-48 md:h-64"}>
+                <div className={"p-6 h-52 md:h-64"}>
                     <h3 className={"text-xl font-bold text-white mb-2"}>{post.title}</h3>
                     <p className={"text-gray-300 text-sm mb-4"}>{new Date(post.date).toLocaleDateString()}</p>
                     <div className={"flex space-x-2"}>
                         {Array.isArray(post.tags) && post.tags.map((tag: string, tagIndex: number) => (
                             <span key={tagIndex} className={"bg-blue-500 text-white px-2 py-1 rounded-full text-xs"}>
-                            {tag}
-                        </span>
+                                {tag}
+                            </span>
                         ))}
                     </div>
                 </div>
@@ -72,12 +72,12 @@ const PostCard = ({ post, index }: PostCardProps) => {
                     objectPosition={"center"}
                     className={cn(
                         "transition duration-300 ease-in-out",
-                            cardHover ? "transform scale-125" : ""
+                        isHovered ? "transform scale-125" : ""
                     )}
                 />
             </div>
-        </Link>
-    )
-}
+        </div>
+    );
+};
 
 export default PostCard;
