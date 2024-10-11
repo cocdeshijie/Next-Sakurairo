cce9641d059b6914c785ef46c06d549b2db42656
cocdeshijie <me@qwq.xyz>
2024-10-11 01:37:29 -0400

    mdx blockquote

--- src/components/ui/MDX/index.tsx
+++ src/components/ui/MDX/index.tsx
@@ -7,9 +7,9 @@ import { tableComponents } from "@/components/ui/MDX/MDXTable";
 import { listComponents } from "@/components/ui/MDX/MDXList";
 
 const useMDXComponents = (code: string) => {
-    const fn = new Function(code)
-    return fn({ ...runtime }).default
-}
+    const fn = new Function(code);
+    return fn({ ...runtime }).default;
+};
 
 const components = {
     h1: H1,
@@ -18,19 +18,46 @@ const components = {
     h4: H4,
     h5: H5,
     h6: H6,
-    p: ({ className, children, ...props }: { className?: string; children: React.ReactNode }) => (
+    p: ({
+            className,
+            children,
+            ...props
+        }: {
+        className?: string;
+        children: React.ReactNode;
+    }) => (
         <p className={cn("text-theme-950 dark:text-theme-50", className)} {...props}>
             {children}
         </p>
     ),
     a: MDXLink,
+    blockquote: ({
+                     className,
+                     children,
+                     ...props
+                 }: {
+        className?: string;
+        children: React.ReactNode;
+    }) => (
+        <blockquote
+            className={cn(
+                "border-l-4 border-theme-500/75 pl-4 py-0.5 rounded-r-md",
+                "shadow shadow-theme-800/15 dark:shadow-theme-200/15",
+                "text-theme-800 dark:text-theme-200 bg-theme-200/50 dark:bg-theme-800/50",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </blockquote>
+    ),
     ...tableComponents,
-    ...listComponents
+    ...listComponents,
 };
 
 const sharedComponents = {
     MDXCode,
-    ...components
+    ...components,
 };
 
 interface MDXProps {
@@ -42,7 +69,5 @@ interface MDXProps {
 export function MDX({ code, components, ...props }: MDXProps) {
     const Component = useMDXComponents(code);
 
-    return (
-        <Component components={{ ...sharedComponents }} {...props} />
-    );
+    return <Component components={{ ...sharedComponents }} {...props} />;
 }
